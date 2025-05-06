ff34e0454881c7ce99f4b60a1cf82bdd194b84e0
baripembo <erikawei@gmail.com>
2025-05-06 16:33:52 -0400

    ruff fix

--- src/hdx/scraper/enso/__main__.py
+++ src/hdx/scraper/enso/__main__.py
@@ -10,12 +10,13 @@ from os.path import dirname, expanduser, join
 
 from hdx.api.configuration import Configuration
 from hdx.facades.infer_arguments import facade
-from hdx.scraper.enso.enso import ENSO
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import (
     wheretostart_tempdir_batch,
 )
 from hdx.utilities.retriever import Retrieve
+
+from hdx.scraper.enso.enso import ENSO
 
 logger = logging.getLogger(__name__)
 
